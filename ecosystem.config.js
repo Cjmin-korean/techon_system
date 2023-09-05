f2d35c652d1daeec4ecc93f37a5c9296b1b5df3a
Cjmin-korean <jmin9003@naver.com>
2023-09-05 21:26:36 +0900

    ac

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+      {
+        name: 'app1',
+        script: 'index.js',
+        env: {
+          NODE_ENV: 'production'
+        }
+      },
+      {
+        name: 'app2',
+        script: 'index.js',
+        env: {
+          NODE_ENV: 'production'
+        }
+      }
+    ]
+  };
