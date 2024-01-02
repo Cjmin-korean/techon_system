1302b7d4f88d7ed26662a5dbb69c3339c51c44a0
Cjmin-korean <jmin9003@naver.com>
2024-01-02 11:11:11 +0900

    240102

--- routes/index.js
+++ routes/index.js
@@ -1201,7 +1201,7 @@ module.exports = function (app) {
                     "       i.modelname, " +
                     "       i.itemname, " +
                     "       i.itemprice, " +
-                    "       COALESCE(SUM(ROUND((mi.rollprice / (mi.length * 1000 * (FLOOR(mi.usewidth / bm.materialwidth)) * bm.cavity * (1 + (bm.loss / 100)) / ((bm.onepid + bm.talength + bm.twopid) / bm.allta)) / bm.cavity), 2)), 0) as cost,                    " +
+                    "      COALESCE(SUM(ROUND((mi.rollprice / (mi.length * 1000 * (FLOOR(mi.usewidth / bm.materialwidth)) * bm.cavity * (1 + (bm.loss / 100)) / ((bm.onepid + bm.talength + bm.twopid) / bm.allta)) ), 2)), 5) as cost,                    " +
                     "       CASE " +
                     "        WHEN i.itemprice = 0 THEN 0 " +
                     "        ELSE (COALESCE(SUM(ROUND((mi.rollprice / (mi.length * 1000 * (FLOOR(mi.usewidth / bm.materialwidth)) * bm.cavity * (1 + (bm.loss / 100)) / ((bm.onepid + bm.talength + bm.twopid) / bm.allta)) / bm.cavity), 2)), 0) / i.itemprice) * 100 " +
