bee93d2006ef2fd007032f8c79657f5bb21d4ad6
Cjmin-korean <jmin9003@naver.com>
2024-03-20 15:42:28 +0900

    240102320

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get("/accountupdatedata", cors(), (req, res) => {
     res.sendFile(__dirname + "/views/html/accountinformation.html");
 });
 app.get("/mmp", cors(), (req, res) => {
-    res.sendFile(__dirname + "/views/html/13.html");
+    res.sendFile(__dirname + "/views/html/ipadmain.html");
 });
 
 app.get("/house", cors(), (req, res) => {
