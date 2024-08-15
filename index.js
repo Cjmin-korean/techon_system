ca50cfa61932084902b95e24b068b5cb2aaeca90
Cjmin-korean <jmin9003@naver.com>
2024-08-15 11:41:30 +0900

    24010813

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 
+const app = express();
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
